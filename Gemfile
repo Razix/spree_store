387249e170e1da4a15ff584ad3f9d81d207e9508
Razix <uscreamrazix@gmail.com>
2013-10-24 18:11:56 +0300

    add small overrides

--- Gemfile
+++ Gemfile
@@ -51,3 +51,4 @@ end
 gem 'spree', '2.1.2'
 gem 'spree_gateway', :git => 'https://github.com/spree/spree_gateway.git', :branch => '2-1-stable'
 gem 'spree_auth_devise', :git => 'https://github.com/spree/spree_auth_devise.git', :branch => '2-1-stable'
+gem 'spree_fancy', :git => 'git://github.com/spree/spree_fancy.git', :branch => '2-1-stable'
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ SpreeSt::Application.routes.draw do
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
   mount Spree::Core::Engine, :at => '/'
+  get "/sale", to: "spree/home#sale", as: :sale
           # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
